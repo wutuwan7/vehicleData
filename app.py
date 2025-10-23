3d1f319b4b18babee92ae11b049227f47b687a2c
Kah Yee Shia <shiakahyee@gmail.com>
2025-10-23 15:05:14 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -8,20 +8,37 @@ data_frame = st.file_uploader("Chooose a CSV file", type=["csv"])
 if data_frame is not None:
     st.write("Data Preview:")
     df = pd.read_csv(data_frame)
+    df.columns = df.columns.str.strip()
+
     st.dataframe(df)
 
-    st.write("Line Chart of Tank Capacity and Standard Comsumption (km/litre):")
-
     required_cols = ['VehicleTankCapacity', 'StandardComsumptionKMPerLitre']
-    if all(col in df.columns for col in required_cols):
-        data = df[required_cols]
+    model_col = 'VehicleModelName'
 
-        st.subheader("Line Chart: Tank Capacity & Standard Consumption")
-        st.line_chart(data)
-    else:
-        st.error("The file must contain 'VehicleTankCapacity' and 'StandardComsumptionKMPerLitre' columns.")
+    if model_col not in df.columns:
+        df[model_col] = [f"Vehicle {i+1}" for i in range(len(df))]
 
+    sort_option = st.checkbox("Sort by better fuel efficiency")
+    if sort_option:
+        df = df.sort_values(by='StandardComsumptionKMPerLitre', ascending=False)
+    
+    column_display_map = {
+        'Vehicle Model Name': 'VehicleModelName',
+        'Vehicle Tank Capacity': 'VehicleTankCapacity',
+    }
+
+    x_axis_option = st.selectbox(
+        "Select X-axis:",
+        options=list(column_display_map.keys()),  
+        index=0
+    )
+
+    selected_column = column_display_map[x_axis_option]
+    chart_data = df[[selected_column, 'StandardComsumptionKMPerLitre']]
+    chart_data = chart_data.set_index(selected_column)
+
+    st.line_chart(chart_data)
 
 else:
-    st.info("Please upload a file to begin.")       
+    st.info("Please upload a file to begin.")
 
