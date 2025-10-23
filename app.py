8e422998d388e71e2bb7c79c9eec1990c78453c8
ky <shiakahyee@gmail.com>
2025-10-23 17:35:18 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.title("Vehicle Data Line Chart")
 
-data_frame = st.file_uploader("Choose a CSV file", type=["csv"])
+data_frame = st.file_uploader("Chooose a CSV file", type=["csv"])
 
 if data_frame is not None:
     st.write("Data Preview:")
@@ -19,22 +19,11 @@ if data_frame is not None:
 
     if model_col not in df.columns:
         df[model_col] = [f"Vehicle {i+1}" for i in range(len(df))]
-
-    sort_option = st.selectbox(
-        "Sort data by:",
-        options=[
-            "None",
-            "Better Fuel Efficiency (Descending)",
-            "Vehicle Model Name (A–Z)"
-        ],
-        index=0
-    )
-
-    if sort_option == "Better Fuel Efficiency (Descending)":
+        
+    sort_option = st.checkbox("Sort by Better Fuel Efficiency")
+    if sort_option:
         df = df.sort_values(by='StandardComsumptionKMPerLitre', ascending=False)
-    elif sort_option == "Vehicle Model Name (A–Z)":
-        df = df.sort_values(by='VehicleModelName', ascending=True)
-
+    
     column_display_map = {
         'Vehicle Model Name': 'VehicleModelName',
         'Vehicle Tank Capacity': 'VehicleTankCapacity',
@@ -42,7 +31,7 @@ if data_frame is not None:
 
     x_axis_option = st.selectbox(
         "Select X-axis:",
-        options=list(column_display_map.keys()),
+        options=list(column_display_map.keys()),  
         index=0
     )
 
@@ -50,8 +39,10 @@ if data_frame is not None:
     chart_data = df[[selected_column, 'StandardComsumptionKMPerLitre']]
     chart_data = chart_data.set_index(selected_column)
 
-    st.write("Line Chart Based on Selected X-axis (Filtered out data with null standard comsumption):")
+
+    st.write("Line Chart Based on Selected X-axis (Filtered out data with null standard comsumption ): ")
     st.line_chart(chart_data)
 
 else:
     st.info("Please upload a file to begin.")
+
